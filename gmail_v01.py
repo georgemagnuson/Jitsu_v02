c95b6c8cd20410fa4bff182d6ae70d788376233a
georgemagnuson <40564127+georgemagnuson@users.noreply.github.com>
2022-05-03 21:41:43 +1200

    testing jeremeyephron/simplegmail instead

--- gmail_v01.py
+++ gmail_v01.py
@@ -2,18 +2,20 @@
 """
 Author : George Magnuson <georgemagnuson@gmail.com>
 Date   : 2022 05 03
-Purpose: try out charlierguo/gmail
+Purpose: try out jeremyephron/simplegmail
 """
 
 import argparse
-import gmail
-
+from simplegmail import Gmail
+import rich
 # --------------------------------------------------
+
+
 def get_args():
     """Get command-line arguments"""
 
     parser = argparse.ArgumentParser(
-        description='try out charlierguo/gmail',
+        description='try out jeremyephron/simplemail',
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
     parser.add_argument('positional',
@@ -35,15 +37,33 @@ def get_args():
 def main():
     """Make a jazz noise here"""
 
-    args = get_args()
-    user_arg = args.positional
-    password_arg = args.password
+    # args = get_args()
+    # username = args.positional
+    # password = args.password
 
-    print(f'user_arg = "{user_arg}"')
-    print(f'password_arg = "{password_arg}"')
+    # print(f'username = "{username}"')
+    # print(f'password = "{password}"')
 
-    # g = gmail.login(username, password)
+    gmail = Gmail()
 
+    # Unread messages in your inbox
+    messages = gmail.get_unread_inbox()
+    rich.inspect(messages)
+    # Starred messages
+    # messages = gmail.get_starred_messages()
+
+    # ...and many more easy to use functions can be found in gmail.py!
+
+    # Print them out!
+    for message in messages:
+        rich.inspect(message)
+        print("To: " + message.recipient)
+        print("From: " + message.sender)
+        print("Subject: " + message.subject)
+        print("Date: " + message.date)
+        print("Preview: " + message.snippet)
+
+        print("Message Body: " + message.plain)  # or message.html
 
 # --------------------------------------------------
 if __name__ == '__main__':
